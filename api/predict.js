33f1a404c59243ccda26419ea83385100686be70
Ted Harvey Condesa <teddddd098@gmail.com>
2026-01-17 05:29:24 +0800

    Fix formidable usage in predict.js

--- api/predict.js
+++ api/predict.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  const form = new formidable.IncomingForm({ multiples: false });
+const form = formidable({ multiples: false });
 
   form.parse(req, async (err, fields, files) => {
     if (err) {

0c358590c3f7a3a4220dce70706432154b74a544
Ted Harvey Condesa <teddddd098@gmail.com>
2026-01-17 07:13:09 +0800

    analyze.js restoration

--- api/analyze.js
+++ api/analyze.js
@@ -1,22 +1,13 @@
 import fetch from "node-fetch"; // IMPORTANT: required in Vercel Node runtime
 
-// This points to your LOCAL backend via ngrok
-const INFERENCE_URL =
+const INFERENCE_API_URL =
   process.env.INFERENCE_API_URL || "http://127.0.0.1:5000/predict";
 
-// Only allow formats your ML model can handle
-const ALLOWED_EXTENSIONS = ["png", "jpg", "jpeg"];
 export const config = {
   api: {
     bodyParser: false,
   },
 };
-
-const response = await fetch(INFERENCE_API_URL, {
-  method: "POST",
-  body: formData,
-  duplex: "half",
-});
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
